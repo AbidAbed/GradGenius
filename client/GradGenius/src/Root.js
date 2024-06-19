04f01a6cde3c9ce1277c3ad35ccdf0e453ce0f53
Abid <abidabed06@gmail.com>
2024-06-19 19:32:07 +0300

    Rebuild app config

--- client/GradGenius/src/Root.js
+++ client/GradGenius/src/Root.js
@@ -1,2 +1,12 @@
-function Root() {}
+import {BASE_BACKEND_URL} from '@env';
+import {Text, View} from 'react-native';
+function Root() {
+  return (
+    <View>
+      <Text>
+        {BASE_BACKEND_URL}
+      </Text>
+    </View>
+  );
+}
 export default Root;
